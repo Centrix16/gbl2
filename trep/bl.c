966c5a9321e792547e5c5f392f375ee7c7797143
Centrix16 <tel1tsyn@yandex.ru>
2020-07-13 17:14:30 +0300

    add ==

--- trep/bl.c
+++ trep/bl.c
@@ -12,8 +12,8 @@
 
 #include "proto.h"
 
-char *built_in[] = {"output", "let", ";", "input", "~", "exit", "+", "-", "*", "/", "eval", ">=", "<=", ">", "<"};
-void (*built_in_funcs[])(unit*) = {output, let, no_eval, input, comment, quit, sum, sub, mul, divop, eval, more_or_equal, less_or_equal, more, less};
+char *built_in[] = {"output", "let", ";", "input", "~", "exit", "+", "-", "*", "/", "eval", ">=", "<=", ">", "<", "=="};
+void (*built_in_funcs[])(unit*) = {output, let, no_eval, input, comment, quit, sum, sub, mul, divop, eval, more_or_equal, less_or_equal, more, less, equal};
 
 extern elm *var_stack;
 extern int line;
@@ -362,6 +362,23 @@ void less(unit *uptr) {
 			break;
 		else
 			result = atof(get_child(uptr, i)->value) < atof(get_child(uptr, i+1)->value);
+
+	sprintf(result_str, "%d", result);
+	strcpy(uptr->value, result_str);
+}
+
+void equal(unit *uptr) {
+	int result = 0;
+	char result_str[64];
+
+	if (uptr->child_num == 1)
+		result = atof(get_child(uptr, 0)->value) == result;
+
+	for (int i = 0; i < uptr->child_num - 1; i++)
+		if (!result && i)
+			break;
+		else
+			result = atof(get_child(uptr, i)->value) == atof(get_child(uptr, i+1)->value);
 
 	sprintf(result_str, "%d", result);
 	strcpy(uptr->value, result_str);
