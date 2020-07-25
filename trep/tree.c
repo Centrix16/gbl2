9aec83bab664a23082665f17d4059fe12cf67847
Centrix14 <artiom.zvyagin@yandex.ru>
2020-07-25 13:25:17 +0300

    added ret_value field

--- trep/tree.c
+++ trep/tree.c
@@ -1,7 +1,7 @@
 /*
  * tree.c -- tree library for trep interpreter
- * v0.0
- * 30.06.2020
+ * v0.2
+ * 27.07.2020
  * by asz
  */
 
@@ -12,10 +12,11 @@
 #include "proto.h"
 
 size_t memory = 0;
+int force_crawl = 0;
 
-void init_unit(unit *uptr, unit *new_parent) {
+void unit_init(unit *uptr, unit *new_parent) {
 	uptr->child_num = 0;
-	uptr->eval_me = 1;
+	uptr->ret_value = NULL;
 
 	strcpy(uptr->value, "\0");
 
@@ -25,7 +26,7 @@ void init_unit(unit *uptr, unit *new_parent) {
 		uptr->parent = new_parent;
 }
 
-void new_child(unit *parent) {
+void unit_new_child(unit *parent) {
 	int child_index = parent->child_num;
 	unit *uptr = NULL;
 
@@ -42,25 +43,21 @@ void new_child(unit *parent) {
 	}
 
 	parent->child[child_index] = uptr;
-	init_unit(parent->child[child_index], parent);
+	parent->child_num++;
+	unit_init(parent->child[child_index], parent);
 }
 
-void set_value(unit *uptr, char *new_value) {
+void unit_set_value(unit *uptr, char *new_value) {
 	strcpy(uptr->value, new_value);
 }
 
-unit *get_child(unit *parent, int child_index) {
+unit *unit_get_child(unit *parent, int child_index) {
 	if (child_index < parent->child_num && child_index > -1)	
 		return parent->child[child_index];
 	return NULL;
 }
 
-void set_end(unit *uptr) {
-	uptr->child_num = 0;
-	uptr->child[0] = NULL;
-}
-
-int is_end_unit(unit *uptr) {
+int unit_is_end(unit *uptr) {
 	if ((uptr->child_num) == 0)
 		return 1;
 	return 0;
@@ -72,29 +69,65 @@ void crawl_tree(unit *parent, void (*func)(unit*)) {
 	if (!uptr)
 		return ;
 
-	if (is_end_unit(uptr)) {
+	if (!strcmp(uptr->value, ";") && !force_crawl)
+		return ;
+
+	if (unit_is_end(uptr)) {
 		(*func)(uptr);
 		return ;
 	}
 
 	for (int i = 0; i < uptr->child_num; i++)
-		crawl_tree(get_child(uptr, i), func);
+		crawl_tree(unit_get_child(uptr, i), func);
 	(*func)(uptr);
 }
 
 
-void show_tree(unit *uptr) {
+void unit_show(unit *uptr) {
 	if (uptr)
 		printf("%s: %s (parent = %p)\n", __func__, uptr->value, uptr->parent);
 }
 
-void del_tree(unit *uptr) {
+void unit_free(unit *uptr) {
 	if (uptr->parent) {
+		if (uptr->ret_value)
+			free(uptr->ret_value);
+
 		free(uptr);
 		memory -= sizeof(unit);
 	}
 }
 
-int get_i(unit *uptr) {
+int unit_get_i(unit *uptr) {
 	return uptr->child_num;
+}
+
+void unit_set_ret_value(unit *uptr, char *value) {
+	char *sptr = NULL;
+
+	if (uptr->ret_value) {
+		sptr = realloc(uptr->ret_value, (strlen(value)+1) * sizeof(char));
+		if (!sptr) {
+			perror(__func__);
+			exit(0);
+		}
+
+		uptr->ret_value = sptr;
+	}
+	else {
+		uptr->ret_value = malloc((strlen(value)+1) * sizeof(char));
+		if (!uptr->ret_value) {
+			perror(__func__);
+			exit(0);
+		}
+	}
+	strcpy(uptr->ret_value, value);
+}
+
+void free_tree(unit *uptr) {
+	force_crawl = 1;
+
+	crawl_tree(uptr, unit_free);
+
+	force_crawl = 0;
 }
